19fb91010a6d01d0b3d75c78736bbd49db2495f2
Alex <abreskolenko@yandex.ru>
2023-02-16 20:05:29 +0300

    Create task 10

---
+++ Task_10/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+ 
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.Clear();
+
+Console.Write("Введите трёхзначное чило : ");
+int number = int.Parse(Console.ReadLine());
+
+number /= 10;
+number %= 10;
+Console.WriteLine(number);
+
